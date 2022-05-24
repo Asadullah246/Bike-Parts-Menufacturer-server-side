e0f2dd724920be7ebb5420b727c9f66b7e5eef3a
Asadullah246 <asadm2258@gmail.com>
2022-05-24 13:19:25 +0600

    order written

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
 const express=require('express');
 const cors=require('cors');
+const app=express();
+const jwt=require('jsonwebtoken');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port=process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j7shq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const partsCollection = client.db("bikePartsData").collection("parts");
+        const orderCollection = client.db("bikePartsData").collection("orders");
+        app.get('/parts', async (req, res) => {
+            const parts=await partsCollection.find({}).toArray();
+            res.send(parts);
+        })
+        app.get('/parts/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const part=await partsCollection.findOne(query);
+            res.send(part);
+        })
+        app.post('/orders',async(req,res)=>{
+            const order=req.body;
+            const insertOrder=await partsCollection.insertOne(order);
+            res.send(insertOrder);
+        })
+
+        app.put("/parts/:id", async (req, res) => {
+            const id = req.params.id;
+            const newQuantity = req.body.quantity;
+            console.log(newQuantity);
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
+            const updateQuantity = {
+                $set: {
+                    quantity: newQuantity
+
+                }
+
+            };
+            const update = await partsCollection.updateOne(filter, updateQuantity, option)
+            res.send(update)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res)=>{
+    res.send('backend running');
+})
+app.listen(port, ()=>{
+    console.log("listening this ", port);
+})
