aab7687237db403864177e821da839ed7b62639b
Asadullah246 <asadm2258@gmail.com>
2022-05-25 08:50:10 +0600

    manage orders added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
-const jwt=require('jsonwebtoken');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
@@ -15,40 +15,44 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j7shq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const partsCollection = client.db("bikePartsData").collection("parts");
         const orderCollection = client.db("bikePartsData").collection("orders");
 
         app.get('/parts', async (req, res) => {
-            const parts=await partsCollection.find({}).toArray();
+            const parts = await partsCollection.find({}).toArray();
             res.send(parts);
         })
-        app.post('/parts', (req, res)=>{
-            const newPart=req.body;
+        app.post('/parts', (req, res) => {
+            const newPart = req.body;
             console.log(newPart);
             partsCollection.insertOne(newPart);
             res.send(newPart);
         })
-        app.post('/parts')
-        app.get('/parts/:id',async(req,res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const part=await partsCollection.findOne(query);
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const part = await partsCollection.findOne(query);
             res.send(part);
         })
-        app.get('/orders',async (req, res)=>{
-            const email=req.headers.email;
-            const query={email:email};
-            const orders=await orderCollection.find(query).toArray();
+        app.get('/allOrders', async (req, res) => {
+            const allOrders = await orderCollection.find({}).toArray();
+            res.send(allOrders);
+
+        })
+        app.get('/orders', async (req, res) => {
+            const email = req.headers.email;
+            const query = { email: email };
+            const orders = await orderCollection.find(query).toArray();
             res.send(orders);
 
         })
-        app.post('/orders',async(req,res)=>{
-            const order=req.body;
-            const insertOrder=await orderCollection.insertOne(order);
-            res.send({success:true});
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const insertOrder = await orderCollection.insertOne(order);
+            res.send({ success: true });
         })
 
         app.put("/parts/:id", async (req, res) => {
@@ -56,35 +60,35 @@ async function run(){
             const newQuantity = req.body.quantity;
             const filter = { _id: ObjectId(id) }
             const option = { upsert: true };
-            if(newQuantity>0){
+            if (newQuantity > 0) {
                 const updateQuantity = {
                     $set: {
                         quantity: newQuantity
-    
+
                     }
-    
+
                 };
                 const update = await partsCollection.updateOne(filter, updateQuantity)
-                res.send({success:true})
+                res.send({ success: true })
 
             }
-            else{
-                res.send({error:"Quantity cannot be negative"})
+            else {
+                res.send({ error: "Quantity cannot be negative" })
             }
-            
+
         })
 
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('backend running');
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("listening this ", port);
 })
